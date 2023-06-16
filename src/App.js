8b092b205f95687c74c5f7ea5fdf96bdb969ad66
ChrisMKocabas <muhammedkocabas@outlook.com>
2023-06-15 22:01:55 -0400

    Front-end demo concept work completed

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
+library.add(fab, fas, far);
