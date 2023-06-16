6fd33f67be8795880e2c62e5298d3f4eb374bb52
ChrisMKocabas <muhammedkocabas@outlook.com>
2023-06-15 22:29:00 -0400

    Github pages configured

---
+++ build/precache-manifest.435721fa8f3907a2584fd5e075ef428c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab45a1096011fc586d4f4232b7e5f50",
+    "url": "/github.io/camper-react-frontend/index.html"
+  },
+  {
+    "revision": "28167f4ab65d6e8268f1",
+    "url": "/github.io/camper-react-frontend/static/css/main.c982f767.chunk.css"
+  },
+  {
+    "revision": "86782077c0c5965bf415",
+    "url": "/github.io/camper-react-frontend/static/js/2.e1b2fd99.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/github.io/camper-react-frontend/static/js/2.e1b2fd99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28167f4ab65d6e8268f1",
+    "url": "/github.io/camper-react-frontend/static/js/main.3f69b4e8.chunk.js"
+  },
+  {
+    "revision": "8313eeef29cca23bc3e5",
+    "url": "/github.io/camper-react-frontend/static/js/runtime-main.041ed325.js"
+  }
+]);
